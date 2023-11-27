6df7ffe79356fe165285b8cad18f0e14286c1944
Angel Villasante <Angel.VillasanteAmaran@tlgcohort.com>
2023-11-27 09:33:28 -0800

    Player can choose to play New Game, fixed AB#85
    - Added Model Classes:
      . Character, it represents a game character
      . Entity, an abstract class that all Objects int he model extend
      . Inventory, a collection of Items
      . Item, represents a game item
      . Room, a game Room
      . Weapon, an item that can be used as a weapon
      . NotImplementedException, an exception to throw when a method is not yet implemented

---
+++ src/com/game/model/Room.java
@@ -0,0 +1,8 @@
+package com.game.model;
+
+/*
+ * Room represents a room in the game, like the Kitchen, the Living Room etc
+ */
+class Room extends Entity{
+
+}
---
+++ src/com/game/model/Weapon.java
@@ -0,0 +1,8 @@
+package com.game.model;
+
+/*
+ * A weapon is a deadly Item
+ */
+class Weapon extends Item{
+    //TODO: Add Weapon logic
+}
